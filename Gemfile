3a5b0abd345571bc255e3fee0af351dc3e0b7c18
Rich Niles <richkniles@gmail.com>
2012-06-10 09:38:41 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.5'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'pg', '0.12.2'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '3.2.4'
+  gem 'coffee-rails', '3.2.2'
+
+  gem 'uglifier', '1.2.3'
+end
+
+gem 'jquery-rails', '2.0.0'
+
+group :test do
+  gem 'capybara', '1.1.2'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PsqlApp::Application.config.secret_token = 'b41707d486c57b9f5b834bd588fa03248e6a4601fa616690bfa2f5b502c6b5eb6df2bf9aade1a8b61d9388ed4132633a78db287b8b0ba74538047ef68d9ebac2'
